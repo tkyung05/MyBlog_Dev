67553f5cf03cae83d838a2aad3560985848e10e9
tkyung05 <tkyung6761@gmail.com>
2022-05-30 11:10:22 +0900

    블로그 포스트 리스트 페이지에 대표 이미지 띄우기

--- my_blog_prj/urls.py
+++ my_blog_prj/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,5 +11,5 @@ urlpatterns = [
 ]
 
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
