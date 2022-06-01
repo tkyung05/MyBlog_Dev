01040fa2861920020d2fd0e8f3904002816af004
tkyung05 <tkyung6761@gmail.com>
2022-06-01 15:18:20 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -117,14 +117,15 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
         self.assertIn(self.post_001.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_oop.name, post_area.text)
 
         self.assertIn(self.user_trump.username.upper(), post_area.text)
-
         self.assertIn(self.post_001.content, post_area.text)
 
