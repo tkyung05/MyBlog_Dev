a1ee6c00b500a64db24c157ab795d772486bf60f
tkyung05 <tkyung6761@gmail.com>
2022-06-04 13:20:44 +0900

    content 입력란에 마크다운 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
+from markdownx.admin import MarkdownxModelAdmin
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,7 +11,7 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 
 admin.site.register(Category, CategoryAdmin)
 
--- my_blog_prj/settings.py
+++ my_blog_prj/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'crispy_forms',
+    'markdownx',
 
     'blog',
     'single_pages',
